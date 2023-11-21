af0ef6360588f79a2d02b913b4a9719685357015
Michiel van der Velde <michiel@michielvdvelde.nl>
2023-11-21 19:40:40 +0100

    feat: add coleman-liau readability utility

---
+++ src/nlp/functions/colemanLiau.ts
@@ -0,0 +1,53 @@
+import { colemanLiau } from "coleman-liau";
+import { AgentFunction, type JSONSchemaType } from "@wecandobetter/delphi";
+import { visit } from "unist-util-visit";
+import retextLatin from "retext-latin";
+import { unified } from "unified";
+
+const processor = unified().use(retextLatin);
+
+export interface ColemanLiauParameters {
+  text: string;
+}
+
+const schema: JSONSchemaType<ColemanLiauParameters> = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      description: "The text to grade.",
+    },
+  },
+  required: ["text"],
+  additionalProperties: false,
+};
+
+const ColemanLiauFunction = new AgentFunction<ColemanLiauParameters, number>(
+  "colemanLiau",
+  "Calculate the ColemanLiau readability index of a text.",
+  schema,
+  async ({ text }) => {
+    let sentences = 0;
+    let words = 0;
+    let letters = 0;
+
+    const root = processor.parse(text);
+
+    visit(root, "SentenceNode", (node) => {
+      sentences++;
+
+      visit(node, "WordNode", (node) => {
+        words++;
+        letters += node.children[0].value.length;
+      });
+    });
+
+    return colemanLiau({
+      sentence: sentences,
+      word: words,
+      letter: letters,
+    });
+  },
+);
+
+export default ColemanLiauFunction;
